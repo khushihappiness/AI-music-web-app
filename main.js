28c29455b1d5518565c1133e4815b4d63d500538
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2021-10-03 14:54:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,22 @@
 function setup()
 {
-    canvas=createCanvas(600, 500);
+    canvas=createCanvas(300, 300);
     canvas.center();
 
     video=createCapture(VIDEO);
     video.hide();
 }
 
-song="";
-song1="";
+music1="";
+music2="";
 
 function preload()
 {
-    song=loadSound("music.mp3")
-    song1=loadSound("music2.mp3")
+    music1=loadSound("Music1.mp3");
+    music2=loadSound("music2.mp3");
 }
 
 function draw()
 {
-    image(video, 0, 0, 600, 500);
-}
-function play()
-{
-    song.play();
-}
-
-function stop()
-{
-    song.stop();
+    image(video, 0, 0, 300, 300)
 }
